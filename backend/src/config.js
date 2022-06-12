efccf7acdc52c76b3d6da8a0c14ab5406cc95a40
fmalavazzi <fmalavazzi@yahoo.com.br>
2022-06-12 15:35:02 -0300

    cmit

--- backend/src/config.js
+++ backend/src/config.js
@@ -55,7 +55,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xAb230ddB4850719b938389b0276aC1D2b2A74f07';
 const TREASURY_ADDRESS = '0xAb230ddB4850719b938389b0276aC1D2b2A74f07';
 const MAX_SUPPLY = 100; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 20; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
